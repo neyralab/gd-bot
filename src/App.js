1ac3f63969cbf179f6ef94fbd435db0d061f1a45
Malinfield <v.malinovskyi@ghostdrive.com>
2023-09-27 12:05:43 +0300

    Prapare frontend for telegram bot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+const tg = window.Telegram.WebApp
 
 function App() {
+
+  useEffect(()=>{
+tg.ready()
+  }, [])
+
+  const onClose = () =>{
+    tg.close()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <button onClick={onClose}>Закрити</button>
     </div>
   );
 }
