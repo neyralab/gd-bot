fded5a0053d3dec1cee5e1126093f3a0df433bae
Victor Malinovskyi <v.malinovskyi@ghostdrive.com>
2023-12-29 13:19:02 +0200

    Add temporary folder for backend part

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { Telegraf, Markup } from "telegraf";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => {
+  const header = "<b>Let's get srarted!</b>";
+  const additionalText =
+    "Please tap the below to start Upload files to/from GhostDrive.";
+  const buttonUrl = "https://tg.beta.ghostdrive.com/";
+  const buttonText = "Open GhostDrive";
+  const button = Markup.button.webApp(buttonText, buttonUrl);
+
+  ctx.replyWithPhoto(
+    { source: fs.createReadStream("./assets/start.png") },
+    {
+      caption: `${header}\n\n${additionalText}`,
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [[button]],
+      },
+    }
+  );
+});
+
+bot.on("text", (ctx) => {
+  ctx.reply("Sorry, command is not recognized");
+});
+
+bot.launch();
+
+app.listen(process.env.PORT, () =>
+  console.log(`My server is running on port ${process.env.PORT}`)
+);
