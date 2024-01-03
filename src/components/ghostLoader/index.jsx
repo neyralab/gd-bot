e279f59a185d84746a88d4dd4744cebf58f58934
Victor Malinovskyi <v.malinovskyi@ghostdrive.com>
2024-01-03 11:02:43 +0200

    Add loader

---
+++ src/components/ghostLoader/index.jsx
@@ -0,0 +1,40 @@
+// Refactoring №3
+import React, { useEffect, useState } from "react";
+
+import { ReactComponent as GhostLogoLoader } from "./svg/ghost-logo-loader.svg";
+
+import classNames from "classnames";
+import s from "./ghostLoader.module.css";
+
+export default function GhostLoader({ texts = [], flashing = true }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (texts.length > 1) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+      }, 5000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [texts]);
+
+  return (
+    <div className={s.wrapper} key={texts[currentIndex]}>
+      <GhostLogoLoader />
+      {texts.length > 0 && (
+        <div className={s.textWrapper}>
+          <p
+            className={classNames(s.textContent, {
+              [s.textContent_isOnePhrase]: texts.length === 1,
+            })}>
+            {texts[currentIndex]}
+          </p>
+          {flashing && <span className={s.blinkingPipe}>|</span>}
+        </div>
+      )}
+    </div>
+  );
+}
