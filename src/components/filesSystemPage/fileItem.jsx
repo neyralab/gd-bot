e2c8944d8e597543c57bef43024a9b614bc84230
Victor Malinovskyi <v.malinovskyi@ghostdrive.com>
2024-01-11 12:00:19 +0200

    Add sharing to the file upload page

--- src/components/filesSystemPage/fileItem.jsx
+++ src/components/filesSystemPage/fileItem.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from "react";
+import { TelegramShareButton } from "react-share";
 
 import moment from "moment";
 
-import { getFilePreviewEffect } from "../../effects/filesEffects";
+import {
+  getFilePreviewEffect,
+  updateShareEffect,
+} from "../../effects/filesEffects";
 import imageFileExtensions, {
   imageMediaTypesPreview,
 } from "../../config/image-file-extensions";
 import videoFileExtensions from "../../config/video-file-extensions";
 
 import CustomFileSmallIcon from "../customFileIcon/CustomFileSmallIcon";
+import { ReactComponent as ShareArrowIcon } from "../../assets/arrow_share.svg";
 
 import style from "./style.module.css";
 
 export const FileItem = ({ file }) => {
   const [preview, setPreview] = useState(null);
+  const url = `https://dev.ghostdrive.com/file/${file.slug}?is_telegram=true`;
 
   const formattedDate = (dateCreated) =>
     moment.unix(dateCreated).format("MMM DD, YYYY, h:mma");
@@ -30,6 +36,11 @@ export const FileItem = ({ file }) => {
     }
   }, []);
 
+  const onShareClick = async (e) => {
+    e.stopPropagation();
+    await updateShareEffect(file.slug);
+  };
+
   return (
     <li className={`${style.options__item} ${style.fileItem}`} key={file.id}>
       <div className={style.fileItem__icon}>
@@ -43,6 +54,13 @@ export const FileItem = ({ file }) => {
         <h3>{file.name}</h3>
         <p>{formattedDate(file.created_at)}</p>
       </div>
+      <TelegramShareButton
+        url={url}
+        title={`Tap this link to see the file "${file.name}"`}
+        className={style.shareButton}
+        onClick={onShareClick}>
+        <ShareArrowIcon />
+      </TelegramShareButton>
     </li>
   );
 };
