0e6219c802bef2ff134371082c072f736a7d2717
Victor Malinovskyi <v.malinovskyi@ghostdrive.com>
2024-06-06 16:35:04 +0300

    Update neyra endpoints

--- src/utils/api-urls.js
+++ src/utils/api-urls.js
@@ -2,3 +2,5 @@ export const API_PATH = `${process.env.REACT_APP_API_PATH}`;
 export const API_AUTHORIZATION = `${process.env.REACT_APP_API_PATH}/tg/auth/telegram`;
 export const API_COUPON = `${process.env.REACT_APP_API_PATH}/use/coupon`;
 export const API_FILE_SHARING = `${process.env.REACT_APP_WEB_FRONTEND_URL}/file`;
+export const API_NEYRA_CONNECT = `${process.env.REACT_APP_NEIRA_AI_API}/auth/identity/connect_userv8`;
+export const API_TON_WALLET = `${process.env.REACT_APP_NEIRA_AI_API}/gateway/billing/retrieve_crypto`;
