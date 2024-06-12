024825169734207f1ccec0029ea0e261d810ccc5
fifa-master <v.vitenko@ghostdrive.com>
2024-06-12 11:04:50 +0300

    data to store

--- src/hooks/useBalance.js
+++ src/hooks/useBalance.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getBalanceEffect } from '../effects/balanceEffect';
+import { setUser } from '../store/reducers/userSlice';
 
 export const useBalance = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state?.user?.data);
   const [balance, setBalance] = useState({
     points: 0,
     fileCnt: 0,
@@ -18,11 +22,12 @@ export const useBalance = () => {
           fileCnt: data.fileCnt,
           history: data.data
         }));
+        user && dispatch(setUser({ ...user, points: data?.points || 0 }));
       } catch (e) {
         console.log({ e });
       }
     })();
-  }, []);
+  }, [dispatch, user]);
 
   return balance;
 };
