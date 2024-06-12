d5e885557f0b1c176c7a2e6cea6bcc36bd23fa98
aidera <hope.aidera@gmail.com>
2024-06-12 10:56:13 -0400

    Optimization attempt 3

--- src/pages/tap/Background/Background.jsx
+++ src/pages/tap/Background/Background.jsx
@@ -18,10 +18,13 @@ const Background = forwardRef(({ theme }, ref) => {
   const speedRef = useRef(0.01); // I'm using ref instead of useState ON PURPOSE! It won't work with useState because of js closures in animate function
   const distanceRef = useRef(0); // I'm using ref instead of useState ON PURPOSE! It won't work with useState because of js closures in animate function
   const lastClickTimeRef = useRef(Date.now()); // I'm using ref instead of useState ON PURPOSE! It won't work with useState because of js closures in animate function
+  const thenRef = useRef(Date.now());
 
   const maxSpeed = 1;
   const decreaseInterval = 2000; //  decrease coef to 0
   const noClickTimeout = 1000;
+  const fps = 40; // The max frame rate
+  const fpsInterval = 1000 / fps; // The interval between frames in milliseconds
 
   const animate = () => {
     if (
@@ -33,13 +36,20 @@ const Background = forwardRef(({ theme }, ref) => {
     )
       return;
 
-    distanceRef.current += speedRef.current; // Increment distance by the current speed
+    const now = Date.now();
+    const elapsed = now - thenRef.current;
 
-    starsRef.current.style.backgroundPosition = `50% ${-distanceRef.current}%`;
-    glowRef.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.1}%`;
-    object1Ref.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.3}%`;
-    object2Ref.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.5}%`;
-    planetRef.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.15}%`;
+    if (elapsed > fpsInterval) {
+      thenRef.current = now - (elapsed % fpsInterval);
+
+      distanceRef.current += speedRef.current; // Increment distance by the current speed
+
+      starsRef.current.style.backgroundPosition = `50% ${-distanceRef.current}%`;
+      glowRef.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.1}%`;
+      object1Ref.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.3}%`;
+      object2Ref.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.5}%`;
+      planetRef.current.style.backgroundPosition = `50% ${-distanceRef.current * 0.15}%`;
+    }
 
     requestAnimationRef.current = requestAnimationFrame(animate);
   };
@@ -57,7 +67,7 @@ const Background = forwardRef(({ theme }, ref) => {
     const intervalId = setInterval(() => {
       if (Date.now() - lastClickTimeRef.current > noClickTimeout) {
         const newSpeed =
-          speedRef.current - (speedRef.current / decreaseInterval) * 50;
+          speedRef.current - (speedRef.current / decreaseInterval) * 100;
         speedRef.current = newSpeed;
 
         if (newSpeed <= 0.01) {
@@ -66,7 +76,7 @@ const Background = forwardRef(({ theme }, ref) => {
           requestAnimationRef.current = null;
         }
       }
-    }, 50);
+    }, 100);
 
     return () => {
       clearInterval(intervalId);
