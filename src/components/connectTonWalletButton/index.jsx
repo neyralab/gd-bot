72556990d5ad4d5008b1e6ba63f1fbeab43067d0
fifa-master <v.vitenko@ghostdrive.com>
2024-06-13 14:17:09 +0300

    added ton proof concept and save wallet to user

--- src/components/connectTonWalletButton/index.jsx
+++ src/components/connectTonWalletButton/index.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
-import { useTonAddress, useTonConnectModal } from '@tonconnect/ui-react';
+import React, { useEffect } from 'react';
+import {
+  useTonAddress,
+  useTonConnectModal,
+  useTonConnectUI,
+  useTonWallet
+} from '@tonconnect/ui-react';
+import { saveUserWallet } from '../../effects/userEffects';
 
 import { ReactComponent as PlusIcon } from '../../assets/plusIcon.svg';
-
 import style from './style.module.scss';
 
 export const ConnectTonWalletButton = ({ openDisconnectModal }) => {
   const address = useTonAddress(true);
   const { open } = useTonConnectModal();
+  const wallet = useTonWallet();
+  const [tonConnectUI] = useTonConnectUI();
+
+  useEffect(() => {
+    (async () => {
+      if (!wallet) {
+        tonConnectUI.setConnectRequestParameters({
+          state: 'ready',
+          value: {
+            tonProof: `Welcome to Neyra Network. Your ID for this signature request is`
+          }
+        });
+      } else {
+        tonConnectUI.setConnectRequestParameters({ state: 'loading' });
+      }
+    })();
+  }, [wallet, tonConnectUI]);
+
+  useEffect(
+    () =>
+      tonConnectUI.onStatusChange(async (wallet) => {
+        console.log({ onStatusChange: wallet });
+        if (
+          wallet?.connectItems?.tonProof &&
+          'proof' in wallet?.connectItems.tonProof
+        ) {
+          const res = await saveUserWallet({
+            tonProof: wallet?.connectItems.tonProof,
+            account: wallet?.account
+          });
+          console.log(wallet?.connectItems, { res });
+        }
+      }),
+    []
+  );
 
   return (
     <div className={style.wrapper}>
