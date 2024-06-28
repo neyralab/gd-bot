c579fb05379c73f44fda546b28e06f58a8afb783
fifa-master <v.vitenko@ghostdrive.com>
2024-06-28 18:37:40 +0300

    remove tonproof

--- src/components/connectTonWalletButton/index.jsx
+++ src/components/connectTonWalletButton/index.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {
   useTonAddress,
   useTonConnectModal,
-  useTonConnectUI,
+  // useTonConnectUI,
   useTonWallet
 } from '@tonconnect/ui-react';
 import { saveUserWallet } from '../../effects/userEffects';
@@ -14,43 +14,44 @@ export const ConnectTonWalletButton = ({ openDisconnectModal }) => {
   const address = useTonAddress(true);
   const { open } = useTonConnectModal();
   const wallet = useTonWallet();
-  const [tonConnectUI] = useTonConnectUI();
-  const [tonProof, setTonProof] = useState();
+  // const [tonConnectUI] = useTonConnectUI();
+  // const [tonProof, setTonProof] = useState();
+  //
+  // useEffect(() => {
+  //   (async () => {
+  //     if (!wallet) {
+  //       tonConnectUI.setConnectRequestParameters({
+  //         state: 'ready',
+  //         value: {
+  //           tonProof: `Welcome to Neyra Network. Your ID for this signature request is`
+  //         }
+  //       });
+  //     } else {
+  //       tonConnectUI.setConnectRequestParameters({ state: 'loading' });
+  //     }
+  //   })();
+  // }, [wallet, tonConnectUI]);
+
+  // useEffect(
+  //   () =>
+  //     tonConnectUI.onStatusChange((wallet) => {
+  //       console.log({ onStatusChange: wallet });
+  //       if (
+  //         wallet?.connectItems?.tonProof &&
+  //         'proof' in wallet?.connectItems.tonProof
+  //       ) {
+  //         setTonProof(wallet?.connectItems.tonProof);
+  //       }
+  //     }),
+  //   [tonConnectUI]
+  // );
 
   useEffect(() => {
-    (async () => {
-      if (!wallet) {
-        tonConnectUI.setConnectRequestParameters({
-          state: 'ready',
-          value: {
-            tonProof: `Welcome to Neyra Network. Your ID for this signature request is`
-          }
-        });
-      } else {
-        tonConnectUI.setConnectRequestParameters({ state: 'loading' });
-      }
-    })();
-  }, [wallet, tonConnectUI]);
-
-  useEffect(
-    () =>
-      tonConnectUI.onStatusChange((wallet) => {
-        console.log({ onStatusChange: wallet });
-        if (
-          wallet?.connectItems?.tonProof &&
-          'proof' in wallet?.connectItems.tonProof
-        ) {
-          setTonProof(wallet?.connectItems.tonProof);
-        }
-      }),
-    [tonConnectUI]
-  );
-
-  useEffect(() => {
-    if (address && tonProof && wallet) {
+    if (address && wallet) {
+      // if (address && tonProof && wallet) {
       (async () => {
         const res = await saveUserWallet({
-          tonProof,
+          // tonProof,
           account: {
             ...wallet?.account,
             uiAddress: address
@@ -59,7 +60,8 @@ export const ConnectTonWalletButton = ({ openDisconnectModal }) => {
         console.log({ res });
       })();
     }
-  }, [address, tonProof, wallet]);
+  }, [address, wallet]);
+  // }, [address, tonProof, wallet]);
 
   return (
     <div className={style.wrapper}>
