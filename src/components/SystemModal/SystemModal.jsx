86aeeeee9a0f2bf17dbc79923eb0e3b0086b617a
aidera <hope.aidera@gmail.com>
2024-07-02 13:57:38 -0400

    Slider modal and system modal added

---
+++ src/components/SystemModal/SystemModal.jsx
@@ -0,0 +1,72 @@
+import React, {
+  forwardRef,
+  useState,
+  useImperativeHandle,
+  useMemo
+} from 'react';
+import Modal from 'react-modal';
+import styles from './SystemModal.module.css';
+
+const SystemModal = forwardRef((_, ref) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [actions, setActions] = useState([]);
+
+  /**  PARAMS:
+   * title: string
+   * text: string
+   * actions: Array of {title: string, text: string, onClick: Fn}
+   * */
+  const open = ({ title, text, actions }) => {
+    setTitle(title);
+    setText(text);
+    setActions(actions);
+    setIsOpen(true);
+  };
+  
+  const close = () => {
+    setIsOpen(false);
+    setTitle('');
+    setText('');
+    setActions([]);
+  };
+
+  useImperativeHandle(ref, () => ({
+    open: open,
+    close: close
+  }));
+
+  const drawActions = useMemo(() => {
+    if (!actions || actions.length <= 0) return null;
+
+    return actions.map((el) => {
+      return (
+        <button
+          key={el.text}
+          className={styles['button-type__' + el.type]}
+          onClick={el.onClick}>
+          {el.text}
+        </button>
+      );
+    });
+  }, [actions]);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={close}
+      shouldCloseOnOverlayClick={true}
+      overlayClassName={styles.overlay}
+      className={styles.modal}
+      ariaHideApp={false}>
+      <div className={styles.content}>
+        <h3 className={styles.title}>{title}</h3>
+        <p className={styles.text}>{text}</p>
+      </div>
+      {actions && <div className={styles.buttons}>{drawActions}</div>}
+    </Modal>
+  );
+});
+
+export default SystemModal;
---
+++ src/pages/earn/EarnModal/EarnModal.jsx
@@ -0,0 +1,153 @@
+import React, {
+  useState,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useMemo,
+} from 'react';
+import { useSelector } from 'react-redux';
+import { Sheet } from 'react-modal-sheet';
+import classNames from 'classnames';
+import { TelegramShareButton } from 'react-share';
+import { ReactComponent as CloseIcon } from '../../../assets/close.svg';
+import { checkTgJoin } from '../../../effects/EarnEffect';
+import SystemModal from '../../../components/SystemModal/SystemModal';
+import styles from './EarnModal.module.css';
+
+const EarnModal = forwardRef(({ item }, ref) => {
+  const link = useSelector((state) => state.user.link);
+  const modalRef = useRef(null);
+  const systemModalRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useImperativeHandle(ref, () => ({
+    open: open
+  }));
+
+  const open = () => {
+    setIsOpen(true);
+  };
+
+  const close = () => {
+    setIsOpen(false);
+  };
+
+  const checkTelegram = async () => {
+    const res = await checkTgJoin();
+    if (!res) {
+      systemModalRef.current.open({
+        title: 'Oops!',
+        text: 'You did not join our YouTube Channel',
+        actions: [
+          {
+            type: 'default',
+            text: 'Try again',
+            onClick: () => {
+              checkTelegram();
+              systemModalRef.current.close();
+            }
+          },
+        ]
+      });
+    } else {
+      systemModalRef.current.open({
+        title: 'Success!',
+        text: 'You joined our YouTube Channel',
+        actions: [
+          {
+            type: 'default',
+            text: 'OK',
+            onClick: () => {
+              checkTelegram();
+              systemModalRef.current.close();
+            }
+          }
+        ]
+      });
+    }
+  };
+
+  const drawJoinButton = useMemo(() => {
+    if (!item) return null;
+
+    if (item.id === 'joinTG') {
+      return (
+        <TelegramShareButton url={link.copy}>
+          <div className={classNames(styles.button, styles['join-button'])}>
+            Join
+          </div>
+        </TelegramShareButton>
+      );
+    } else if (item.joinLink) {
+      return (
+        <a
+          href={item.joinLink}
+          target="_blank"
+          className={classNames(styles.button, styles['join-button'])}>
+          {item.id === 'downloadMobileApp' ? 'Download' : 'Join'}
+        </a>
+      );
+    } else {
+      return null;
+    }
+  }, [item]);
+
+  const drawCheckButton = useMemo(() => {
+    if (!item) return null;
+
+    if (item.id === 'joinTG') {
+      return (
+        <button
+          className={classNames(styles.button, styles['check-button'])}
+          onClick={checkTelegram}>
+          Check
+        </button>
+      );
+    } else return null;
+  }, [item]);
+
+  if (!item) return;
+
+  return (
+    <>
+      <Sheet
+        ref={modalRef}
+        isOpen={isOpen}
+        onClose={close}
+        detent="content-height">
+        <Sheet.Container className="react-modal-sheet-container">
+          <Sheet.Header className="react-modal-sheet-header" />
+          <Sheet.Content>
+            <Sheet.Scroller>
+              <div className={styles.container}>
+                <div className={styles.header}>
+                  <div className={styles.points}>
+                    <span>{item.points.toLocaleString()}</span>
+                    <img src="/assets/token.png" />
+                  </div>
+                  <div className={styles.close} onClick={close}>
+                    <CloseIcon />
+                  </div>
+                </div>
+
+                <div className={styles.content}>
+                  <img src={item.imgUrl} alt={item.title} />
+                  <h3>{item.title}</h3>
+                </div>
+
+                <div className={styles.actions}>
+                  {drawJoinButton}
+                  {drawCheckButton}
+                </div>
+              </div>
+            </Sheet.Scroller>
+          </Sheet.Content>
+        </Sheet.Container>
+      </Sheet>
+
+      <SystemModal ref={systemModalRef} />
+    </>
+  );
+});
+
+export default EarnModal;
