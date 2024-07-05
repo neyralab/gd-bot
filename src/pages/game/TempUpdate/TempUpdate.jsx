8fcd4f1ec8572e39c128818fc5caee04c209f938
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-07-05 14:42:18 +0300

    change CountUp

--- src/pages/game/TempUpdate/TempUpdate.jsx
+++ src/pages/game/TempUpdate/TempUpdate.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, memo, useState } from 'react';
+import CountUp from 'react-countup';
 import classNames from 'classnames';
 
 import styles from './TempUpdate.module.css';
@@ -7,7 +8,6 @@ const TIME_OUT = 6000;
 
 const TempUpdate = ({ isActive, counter, onClose }) => {
   const [show, setShow] = useState(true);
-  const [currentCounter, setCrrentCounter] = useState(0);
 
   useEffect(() => {
     if (isActive) {
@@ -27,24 +27,9 @@ const TempUpdate = ({ isActive, counter, onClose }) => {
     }
   }, [isActive]);
 
-  
-  useEffect(() => {
-    if (isActive && currentCounter < counter) {
-      const increment = counter / 100; // increment value per step
-      const interval = setInterval(() => {
-        setCrrentCounter((prevCount) => {
-          const newCount = prevCount + increment;
-          return Math.round(newCount >= counter ? counter : newCount);
-        });
-      }, 10); // 100 steps per second
-
-      return () => clearInterval(interval); // clean up interval on component unmount
-    }
-  }, [currentCounter, counter, isActive]);
-
   return (
     <div className={classNames(styles['temp-update'], (isActive && show) && styles['temp-active'])}>
-      <p className={styles.count}>{currentCounter}</p>
+      <p className={styles.count}><CountUp delay={1} end={counter}/></p>
       <span className={styles[`points-text`]}>points</span>
     </div>
   );
