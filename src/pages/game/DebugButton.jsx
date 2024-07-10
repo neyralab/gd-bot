a4417cb4c2ae31f3ff5e7cd5992f3ed9874f8154
aidera <hope.aidera@gmail.com>
2024-07-10 13:17:38 -0400

    Tainaya knopka

--- src/pages/game/DebugButton.jsx
+++ src/pages/game/DebugButton.jsx
@@ -1,23 +1,41 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 export default function DebugButton() {
   const state = useSelector((state) => state.game);
+  const navigate = useNavigate();
 
-  const clickHandler = () => {
+  const clipboardHandler = () => {
     navigator.clipboard.writeText(JSON.stringify(state));
   };
 
+  const game3DHandler = () => {
+    navigate('/game-3d');
+  };
+
   return (
-    <div
-      onClick={clickHandler}
-      style={{
-        width: '100px',
-        height: '100px',
-        position: 'fixed',
-        top: 0,
-        right: 0,
-        zIndex: '99999999999999999999'
-      }}></div>
+    <>
+      <div
+        onClick={game3DHandler}
+        style={{
+          width: '25px',
+          height: '25px',
+          position: 'fixed',
+          bottom: 0,
+          left: 0,
+          zIndex: '99999999999999999999',
+        }}></div>
+      <div
+        onClick={clipboardHandler}
+        style={{
+          width: '100px',
+          height: '100px',
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          zIndex: '99999999999999999999'
+        }}></div>
+    </>
   );
 }
