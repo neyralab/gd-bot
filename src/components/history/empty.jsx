b5aa5b528e3f6e298f5ba1a4c624844f3626753a
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-07-26 13:43:07 +0300

    add ui update from roma

--- src/components/history/empty.jsx
+++ src/components/history/empty.jsx
@@ -2,13 +2,19 @@ import { useTranslation } from 'react-i18next';
 import { ReactComponent as Clock } from '../../assets/clock.svg';
 import styles from './styles.module.css';
 
-export const NoHistory = () => {
+export const NoHistory = ({ loading }) => {
+  const { t : tSystem } = useTranslation('system');
   const { t } = useTranslation('game');
+  
   return (
     <div className={styles.empty_container}>
       <Clock width={48} height={48} />
-      <p className={styles.header}>{t('airdrop.noHistory')}</p>
-      <p className={styles.text}>{t('airdrop.shareReferral')}</p>
+      <p className={styles.header}>
+        {loading ? tSystem('loading.pleaseWait') : t('airdrop.noHistory')}
+      </p>
+      <p className={styles.text}>
+        {loading ? tSystem('loading.systemLoading') : t('airdrop.shareReferral')}
+      </p>
     </div>
   );
 };
--- src/pages/leaderboard/assets/Header/Header.jsx
+++ src/pages/leaderboard/assets/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 
 import useButtonVibration from '../../../../hooks/useButtonVibration';
 
@@ -9,13 +10,16 @@ import styles from './Header.module.css';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const location = useLocation();
   const { t } = useTranslation('game');
   const user = useSelector((state) => state?.user?.data);
   const handleVibrationClick = useButtonVibration();
 
   return (
     <header className={styles.pageHeader}>
-      <span className={styles.rank}>{`Rank ${user.rank}`}</span>
+      <span className={styles.rank}>
+        { location.pathname === '/leadboard/league' && (`Rank ${user?.rank}`) }
+      </span>
       <div className={styles.navigation}>
         <NavLink
           className={({ isActive }) => classNames(isActive && styles.active)}
