64211107497c99329b7906788edb31b64ea41c17
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-08-20 17:30:56 +0300

    change mobile preview logic

--- src/pages/startPage/ShareStorage/index.jsx
+++ src/pages/startPage/ShareStorage/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { SlidingModal } from '../../../components/slidingModal';
-import { useKeyboardStatus } from '../../../utils/useKeyboardStatus';
 
 import GiftPreview from './Gift';
 import ShareForm from './ShareForm';
+import { isPhone } from "../../../utils/client";
 
 const snapPoints = {
   gift: [420, 420, 50, 0],
@@ -12,18 +12,24 @@ const snapPoints = {
 }
 
 const ShareStorage = ({ isOpen, onClose, giftData, onCloseGift, systemModalRef }) => {
-  const isKeybordOpen = useKeyboardStatus();
+  const [fullScreen, setFullScreen] = useState(false)
+
+  const handleFullScreeView = (state) => {
+    if (isPhone()) {
+      setFullScreen(state);
+    }
+  } 
 
   return (
     <SlidingModal
       onClose={!!giftData.length ? onCloseGift: onClose }
       isOpen={isOpen}
-      snapPoints={ isKeybordOpen ? undefined : (!!giftData.length? snapPoints.gift : snapPoints.form)}
-      detent={isKeybordOpen && "full-height"}
+      snapPoints={ fullScreen ? undefined : (!!giftData.length? snapPoints.gift : snapPoints.form)}
+      detent={fullScreen && "full-height"}
     >
     {!!giftData.length ? (
       <GiftPreview systemModalRef={systemModalRef} giftData={giftData} onCloseGift={onCloseGift} />
-      ) : (<ShareForm onClose={onClose} />
+      ) : (<ShareForm handleFullScreeView={handleFullScreeView} onClose={onClose} />
     )}
     </SlidingModal>
   )
