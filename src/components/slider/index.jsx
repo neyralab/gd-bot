4a989d51527857b2a9320229a156b4349252e9a8
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-08-22 11:02:10 +0300

    rename share title

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,14 +1,37 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useRef, useEffect } from 'react';
 import CN from 'classnames';
-
 import styles from './styles.module.scss';
 
 const Slider = ({ className, value, onChange, maxValue }) => {
   const procent = useMemo(() => ((value / maxValue) * 100).toFixed(1), [value, maxValue]);
+  const sliderRef = useRef(null);
 
   const handleChange = (event) => {
     onChange(Number(event.target.value));
   };
+
+  const handleTouchMove = (event) => {
+    if (sliderRef.current) {
+      const rect = sliderRef.current.getBoundingClientRect();
+      const touch = event.touches[0];
+      const relativeX = touch.clientX - rect.left;
+      const newValue = Math.min(Math.max(0, (relativeX / rect.width) * maxValue), maxValue);
+      onChange(Math.round(newValue));
+    }
+  };
+
+  useEffect(() => {
+    const sliderElement = sliderRef.current;
+    if (sliderElement) {
+      sliderElement.addEventListener('touchmove', handleTouchMove);
+    }
+
+    return () => {
+      if (sliderElement) {
+        sliderElement.removeEventListener('touchmove', handleTouchMove);
+      }
+    };
+  }, [handleTouchMove]);
 
   return (
     <div className={CN(styles["slider-container"], className)}>
@@ -19,6 +42,7 @@ const Slider = ({ className, value, onChange, maxValue }) => {
         value={value}
         className={styles["slider"]}
         onChange={handleChange}
+        ref={sliderRef}
         style={{
           background: `linear-gradient(to right, #fff 0%, #fff ${procent}%, #333 ${procent}%, #333 100%)`,
         }}
