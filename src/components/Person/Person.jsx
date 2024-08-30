6c367d6e06da86da96c5bafb449a736be04d574f
aidera <hope.aidera@gmail.com>
2024-08-30 07:51:56 -0400

    Fixed friends and earn pages animation

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,18 +3,23 @@ import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 import styles from './Person.module.css';
 
-export default function Person({ isDone, title, points, onClick }) {
+export default function Person({ isDone, title, points, onClick, className }) {
   const { t } = useTranslation('game');
   return (
     <div
-      className={classNames(styles.container, isDone && styles.done)}
+      data-animation="person-animation-1"
+      className={classNames(styles.container, isDone && styles.done, className)}
       onClick={onClick}>
       <div className={styles.name}>
         <strong>{title}</strong>
       </div>
+
       <div className={styles.points}>
         <span>+{points}</span>
-        <img src="/assets/token.png" alt={`+ ${points} ${t('leadboard.points')}`} />
+        <img
+          src="/assets/token.png"
+          alt={`+ ${points} ${t('leadboard.points')}`}
+        />
       </div>
     </div>
   );
