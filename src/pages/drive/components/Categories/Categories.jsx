57108fd2533809e398652e1643edba25b50081ea
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-09-18 17:34:43 +0300

    fix categories preview

--- src/pages/drive/components/Categories/Categories.jsx
+++ src/pages/drive/components/Categories/Categories.jsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { assignFilesQueryData } from '../../../../store/reducers/driveSlice';
 import { ReactComponent as ImpulseLoader } from '../../../../assets/loader-impulse.svg';
 import { vibrate } from '../../../../utils/vibration';
+import { isDevEnv } from '../../../../utils/isDevEnv';
 import icons from './assets';
 import { runInitAnimation } from './animations';
 import styles from './Categories.module.scss';
 
+const HIDE_OPTION = ['ppv'];
+
 export default function Categories() {
   const dispatch = useDispatch();
+  const isDev = isDevEnv();
   const types = useSelector((state) => state.drive.fileTypesCount);
   const typesAreFetching = useSelector(
     (state) => state.drive.fileTypesCountIsFetching
@@ -27,65 +31,76 @@ export default function Categories() {
     dispatch(assignFilesQueryData({ filesQueryData: { category: type } }));
   };
 
-  const options = [
-    {
-      name: t('dashbord.all'),
-      value: types?.total,
-      icon: icons.all,
-      callback: () => getFiles('all')
-    },
-    {
-      name: t('dashbord.favorites'),
-      value: types?.starred,
-      icon: icons.fav,
-      callback: () => getFiles('fav')
-    },
-    // {
-    //   name: 'Star Space',
-    //   value: types?.ppv,
-    //   icon: icons.star,
-    //   callback: () => getFiles('payShare')
-    // },
-    {
-      name: t('dashbord.pictures'),
-      value: types?.images,
-      icon: icons.pictures,
-      callback: () => getFiles('image')
-    },
-    {
-      name: t('dashbord.documents'),
-      value: types?.docs,
-      icon: icons.doc,
-      callback: () => getFiles('docs')
-    },
-    {
-      name: t('dashbord.notes'),
-      value: types?.notes,
-      icon: icons.notes,
-      callback: () => getFiles('notes')
-    },
-    {
-      name: t('dashbord.audio'),
-      value: types?.audios,
-      icon: icons.audio,
-      callback: () => getFiles('audio')
-    },
-    {
-      name: t('dashbord.video'),
-      value: types?.videos,
-      icon: icons.video,
-      callback: () => getFiles('video')
-    },
-    {
-      name: t('dashbord.games'),
-      value: types?.games,
-      icon: icons.game,
-      callback: () => {
-        vibrate();
-        navigate('/games');
+  const options = useMemo(() => (
+    [
+      {
+        name: t('dashbord.all'),
+        value: types?.total,
+        indexName: 'all',
+        icon: icons.all,
+        callback: () => getFiles('all')
+      },
+      {
+        name: t('dashbord.favorites'),
+        value: types?.starred,
+        indexName: 'starred',
+        icon: icons.fav,
+        callback: () => getFiles('fav')
+      },
+      {
+        name: 'Star Space',
+        value: types?.ppv,
+        indexName: 'ppv',
+        icon: icons.star,
+        callback: () => getFiles('payShare')
+      },
+      {
+        name: t('dashbord.pictures'),
+        value: types?.images,
+        indexName: 'images',
+        icon: icons.pictures,
+        callback: () => getFiles('image')
+      },
+      {
+        name: t('dashbord.documents'),
+        value: types?.docs,
+        indexName: 'docs',
+        icon: icons.doc,
+        callback: () => getFiles('docs')
+      },
+      {
+        name: t('dashbord.notes'),
+        value: types?.notes,
+        indexName: 'notes',
+        icon: icons.notes,
+        callback: () => getFiles('notes')
+      },
+      {
+        name: t('dashbord.audio'),
+        value: types?.audios,
+        indexName: 'audios',
+        icon: icons.audio,
+        callback: () => getFiles('audio')
+      },
+      {
+        name: t('dashbord.video'),
+        value: types?.videos,
+        indexName: 'videos',
+        icon: icons.video,
+        callback: () => getFiles('video')
+      },
+      {
+        name: t('dashbord.games'),
+        value: types?.games,
+        indexName: 'games',
+        icon: icons.game,
+        callback: () => {
+          vibrate();
+          navigate('/games');
+        }
       }
-    }
-  ];
+    ].filter((item) => isDev ? true : !HIDE_OPTION.includes(item.indexName))
+  ), [isDev, types]);
 
   return (
     <ul
