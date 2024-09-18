39a02c07159d213afeb690fff0b0bba291586ba1
fifa-master <v.vitenko@ghostdrive.com>
2024-09-18 11:39:20 +0300

    Revert to 91f540e6c72f6bc03f9f4e0d7db46197f7c0825c

--- src/pages/startPage/index.jsx
+++ src/pages/startPage/index.jsx
@@ -12,6 +12,7 @@ import { getAllTasks } from '../../effects/balanceEffect';
 import {
   getStorageNotificationsEffect,
   readNotificationEffect,
+  storageSendEffect
 } from '../../effects/storageEffects';
 import { DEFAULT_TARIFFS_NAMES } from '../upgradeStorage';
 import { fromByteToGb } from '../../utils/storage';
@@ -27,9 +28,9 @@ import PointCounter from './PointCounter/PointCounter';
 import SystemModal from '../../components/SystemModal/SystemModal';
 import NavigatItem from './Navigator/NavigatItem';
 import Navigator from './Navigator/Navigator';
-import { ReactComponent as LogoIcon } from '../../assets/ghost.svg';
-import BannerSource from '../../assets/node-banner.webp';
+import { parseSizeToBytes } from '../../utils/storage';
 import { runInitAnimation } from './animations';
+import { Banner } from './Banner';
 
 import style from './style.module.css';
 import navigatorStyle from './Navigator/Navigator.module.scss';
@@ -229,24 +230,11 @@ export const StartPage = ({ tariffs }) => {
 
   return (
     <div ref={wrapperRef} className={`${style.container}`}>
-
-      <div
+      <Banner
+        storageSize={user.space_total}
+        onOpenShareModal={onOpenShareModal}
         data-animation="start-page-animation-2"
-        className={CN(style.card, style.banner)}>
-        <img src={BannerSource} alt="banner" />
-        <div className={style['banner-content']}>
-          <div onClick={onOpenShareModal} className={style['banner-header']}>
-            <div className={style['banner-header_img']}>
-              <LogoIcon />
-              <span className={style['banner-header-share-btn']}>
-                {t('share.share')}
-              </span>
-            </div>
-            <h1>{transformSize(user.space_total)}</h1>
-          </div>
-        </div>
-      </div>
-
+      />
       <PointCounter
         points={user?.points}
         className={style[`point-counter`]}
