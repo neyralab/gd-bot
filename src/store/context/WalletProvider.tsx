d0e39a8d2497b9abfd33352c8a3ec8bd742f56a9
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-09-19 15:20:00 +0300

    okx wallet test

--- src/store/context/WalletProvider.tsx
+++ src/store/context/WalletProvider.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, createContext, useContext, useState, ReactNode } from 'react';
 import { OKXTonConnect, OkxConnectError, OKX_CONNECT_ERROR_CODES } from 'okxconnect';
-import { API_WEB_APP_URL } from '../../utils/api-urls';
+import { API_WEB_APP_URL, BOT_NAME } from '../../utils/api-urls';
 
 interface WalletContextType {
   wallet: OKXTonConnect | null;
@@ -29,8 +29,6 @@ export const WalletProvider: React.FC<WalletProviderProps> = ({ children }) => {
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
-  console.log(wallet);
-
   useEffect(() => {
     try {
       const walletInstance = new OKXTonConnect({
@@ -51,7 +49,7 @@ export const WalletProvider: React.FC<WalletProviderProps> = ({ children }) => {
 
   const connectWallet = async () => {
     try {
-      await wallet?.connect?.({ redirect: '', openUniversalLink: true });
+      await wallet?.connect?.({ redirect: `https://t.me/${BOT_NAME}`, openUniversalLink: true });
     } catch (error: any) {
       if (error instanceof OkxConnectError) {
         if (error.code === OKX_CONNECT_ERROR_CODES.USER_REJECTS_ERROR) {
