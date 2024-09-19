610923e5f6ae30523d19cc05067879bedf765200
aidera <hope.aidera@gmail.com>
2024-09-19 06:52:46 -0400

    Resolve issues

--- src/components/file-previews/components/SliderDots/SliderDots.jsx
+++ src/components/file-previews/components/SliderDots/SliderDots.jsx
@@ -1,21 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './SliderDots.module.scss';
 
 export default function SliderDots({ totalPages, currentPage }) {
   if (totalPages <= 1) return null;
-
-  const dots = [];
-  for (let i = 1; i <= totalPages; i++) {
-    const scale = 1 - Math.min(Math.abs(currentPage - i) * 0.15, 1);
-    const opacity = 1 - Math.min(Math.abs(currentPage - i) * 0.3, 1);
-    dots.push(
-      <div
-        key={i}
-        className={`${styles.dot} ${i === currentPage ? styles.active : ''}`}
-        style={{ transform: `scale(${scale})`, opacity: opacity }}
-      />
-    );
-  }
 
   const containerWidth = 300; // Width of the visible area for dots
   const dotWidth = 14; // Width of each dot including margin
@@ -25,6 +12,22 @@ export default function SliderDots({ totalPages, currentPage }) {
     (currentPage - 1) * dotWidth - containerWidth / 2 + dotWidth / 2,
     maxOffset
   );
+
+  const dots = useMemo(() => {
+    const dotsArray = [];
+    for (let i = 1; i <= totalPages; i++) {
+      const scale = 1 - Math.min(Math.abs(currentPage - i) * 0.15, 1);
+      const opacity = 1 - Math.min(Math.abs(currentPage - i) * 0.3, 1);
+      dotsArray.push(
+        <div
+          key={i}
+          className={`${styles.dot} ${i === currentPage ? styles.active : ''}`}
+          style={{ transform: `scale(${scale})`, opacity: opacity }}
+        />
+      );
+    }
+    return dotsArray;
+  }, [totalPages, currentPage]);
 
   return (
     <div className={styles['dots-container']}>
