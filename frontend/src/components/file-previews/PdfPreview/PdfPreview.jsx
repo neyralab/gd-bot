00b3efed1a250735ed0a482d218a1e5f1c16315b
aidera <hope.aidera@gmail.com>
2024-09-25 10:48:01 -0400

    Added slide animation to PDF

--- frontend/src/components/file-previews/PdfPreview/PdfPreview.jsx
+++ frontend/src/components/file-previews/PdfPreview/PdfPreview.jsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
+import React from 'react';
 import DefaultModeFileUI from '../components/DefaultModeFileUI/DefaultModeFileUI';
-import PdfSnapshotReader from '../components/PdfSnapshotReader/PdfSnapshotReader';
-import SliderDots from '../components/SliderDots/SliderDots';
+import PdfSliderReader from '../components/PdfSliderReader/PdfSliderReader';
 import styles from './PdfPreview.module.scss';
 
 const PdfPreview = ({
@@ -12,35 +10,9 @@ const PdfPreview = ({
   onFavoriteClick,
   onInfoClick
 }) => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(null);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setTotalPages(numPages);
-  };
-
-  const handleSwipe = (deltaX) => {
-    if (deltaX > 0 && currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    } else if (deltaX < 0 && currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlers = useSwipeable({
-    onSwipedLeft: () => handleSwipe(-1),
-    onSwipedRight: () => handleSwipe(1),
-    preventDefaultTouchmoveEvent: true,
-    trackMouse: true
-  });
-
   return (
-    <div className={styles.container} {...handlers}>
-      <PdfSnapshotReader
-        fileContent={fileContent}
-        onDocumentLoadSuccess={onDocumentLoadSuccess}
-        pageNumber={currentPage}
-      />
+    <div className={styles.container}>
+      <PdfSliderReader fileContent={fileContent} />
 
       {mode === 'default' && (
         <DefaultModeFileUI
@@ -49,8 +21,6 @@ const PdfPreview = ({
           onInfoClick={onInfoClick}
         />
       )}
-
-      <SliderDots totalPages={totalPages} currentPage={currentPage} />
     </div>
   );
 };
