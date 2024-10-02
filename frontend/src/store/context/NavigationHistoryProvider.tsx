c80b838d079bf527c5f8f9a65b86a789c93b7fe2
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-10-02 15:11:41 +0300

    add update user info on start page

---
+++ frontend/src/store/context/NavigationHistoryProvider.tsx
@@ -0,0 +1,63 @@
+import React, { createContext, useRef, useState, useEffect, ReactNode } from "react";
+import { useLocation } from "react-router-dom";
+
+interface NavigationHistoryContextType {
+  history: string[];
+  isInitialRoute: boolean;
+}
+
+export const NavigationHistoryContext = createContext<NavigationHistoryContextType | undefined>(undefined);
+
+interface NavigationHistoryProviderProps {
+  children: ReactNode;
+}
+
+export const NavigationHistoryProvider: React.FC<NavigationHistoryProviderProps> = ({ children }) => {
+  const [history, setHistory] = useState<string[]>([]);
+  const [isInitialRoute, setIsInitialRoute] = useState(true);
+  const removedElement = useRef<boolean>(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+
+    setHistory((prevHistory) => {
+      console.log(prevHistory, removedElement.current);
+      if (!removedElement.current) {
+        return [...prevHistory, currentPath];
+      }
+      removedElement.current = false;
+      return prevHistory;
+    });
+  }, [location.pathname]);
+
+  useEffect(() => {
+    if (isInitialRoute && history.length > 1) {
+      setIsInitialRoute(false);
+    }
+  }, [isInitialRoute, history]);
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setHistory((prevHistory) => {
+        if (prevHistory.length > 1) {
+          removedElement.current = true;
+          return prevHistory.slice(0, -1);
+        }
+        return prevHistory;
+      });
+    };
+
+    window.addEventListener("popstate", handlePopState);
+
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+    };
+  }, []);
+
+  return (
+    <NavigationHistoryContext.Provider value={{ history, isInitialRoute }}>
+      {children}
+    </NavigationHistoryContext.Provider>
+  );
+};
