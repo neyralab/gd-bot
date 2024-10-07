78917d2e1ce8231479bb2b8ff62718e72449f7b8
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-10-07 11:48:55 +0300

    simplify code

--- frontend/src/utils/useAdsgram.js
+++ frontend/src/utils/useAdsgram.js
@@ -1,28 +1,22 @@
 import { useCallback } from 'react';
-
 import { AdController } from '../App';
 
 export function useAdsgram({ onReward, onError }) {
-
   return useCallback(async () => {
-    if (AdController) {
-      AdController
-        .show()
-        .then(() => {
-          // user watch ad till the end
-          onReward();
-        })
-        .catch((result) => {
-          // user get error during playing ad or skip ad
-          onError?.(result);
-        });
-    } else {
-      onError?.({
+    if (!AdController) {
+      return onError?.({
         error: true,
         done: false,
         state: 'load',
         description: 'Adsgram script not loaded',
       });
+    }
+
+    try {
+      await AdController.show();
+      onReward();
+    } catch (error) {
+      onError?.(error);
     }
   }, [onError, onReward]);
 }
