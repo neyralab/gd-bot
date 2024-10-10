8ddb7993d4a67699b06b2c7a96440e0f2fd10792
vasylpopovych6 <vasiliy.popovych@ghostdrive.com>
2024-10-10 15:43:27 +0300

    delete useless code

--- frontend/src/store/context/NavigationHistoryProvider.tsx
+++ frontend/src/store/context/NavigationHistoryProvider.tsx
@@ -1,15 +1,5 @@
 import React, { createContext, useRef, useState, useEffect, ReactNode } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  assignFilesQueryData,
-  setMediaSliderOpen,
-  setMediaSliderCurrentFile
-} from '../reducers/driveSlice';
-
-import { isMobilePlatform } from '../../utils/client';
-import { tg } from '../../App';
-
+import { useLocation } from "react-router-dom";
 interface NavigationHistoryContextType {
   history: string[];
   isInitialRoute: boolean;
@@ -24,13 +14,7 @@ interface NavigationHistoryProviderProps {
 export const NavigationHistoryProvider: React.FC<NavigationHistoryProviderProps> = ({ children }) => {
   const [history, setHistory] = useState<string[]>([]);
   const [isInitialRoute, setIsInitialRoute] = useState(true);
-  const queryData = useSelector((state: any) => state.drive.filesQueryData);
-  const mediaSliderIsOpen = useSelector((state: any) => state.drive.mediaSlider.isOpen);
-
   const removedElement = useRef<boolean>(false);
-  const mobilePlatform = isMobilePlatform;
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
   const location = useLocation();
 
   useEffect(() => {
@@ -45,37 +29,6 @@ export const NavigationHistoryProvider: React.FC<NavigationHistoryProviderProps>
     });
   }, [location.pathname]);
 
-  useEffect(() => {
-    if (mobilePlatform) {
-      const goBack = () => {navigate(-1)};
-
-      if (location.pathname === '/start' && tg.BackButton.isVisible) {
-        tg.BackButton.hide();
-      } else if (location.pathname !== '/start' &&
-        !tg.BackButton.isVisible &&
-        location.pathname !== '/drive'
-      ) {
-        tg.BackButton.show();
-        tg.BackButton.onClick(goBack)
-      } else if (location.pathname == '/drive') {
-        !tg.BackButton.isVisible && tg.BackButton.show();
-        if (!!queryData.search || queryData.category !== null) {
-          tg.BackButton.onClick(() => {
-            dispatch(assignFilesQueryData({
-              filesQueryData: { search: null, category: null }
-            }))
-          })
-        } else if (mediaSliderIsOpen) {
-          tg.BackButton.onClick(() => {
-            dispatch(setMediaSliderOpen(false));
-            dispatch(setMediaSliderCurrentFile(null));
-          })
-        } else {
-          tg.BackButton.onClick(goBack)
-        }
-      }
-    }
-  }, [location.pathname, queryData, mediaSliderIsOpen])
 
   useEffect(() => {
     if (isInitialRoute && history.length > 1) {
