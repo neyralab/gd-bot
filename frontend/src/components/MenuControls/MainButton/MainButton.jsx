fc04075339a5f5ba5802bd04164d3aebd8bc4f77
aidera <hope.aidera@gmail.com>
2024-10-24 16:52:08 +0300

    Same actions on all pages

--- frontend/src/components/MenuControls/MainButton/MainButton.jsx
+++ frontend/src/components/MenuControls/MainButton/MainButton.jsx
@@ -1,7 +1,6 @@
 import React, { useMemo, useRef } from 'react';
 import { useLongPress } from 'use-long-press';
 import classNames from 'classnames';
-import { useLocation } from 'react-router-dom';
 
 import { useAssistantAudio } from '../../../pages/assistant/AssistantAudio/AssistantAudio';
 import UploadAction from '../../UploadAction/UploadAction';
@@ -22,12 +21,7 @@ export default function MainButton() {
     stopAudio,
     audioPlayerRef
   } = useAssistantAudio();
-  const location = useLocation();
   const uploadRef = useRef(null);
-  const isAssistantPage = useMemo(
-    () => location.pathname === '/assistant',
-    [location]
-  );
 
   const stopAllAudioActions = () => {
     vibrate();
@@ -49,24 +43,8 @@ export default function MainButton() {
     uploadRef.current.triggerUpload();
   };
 
-  /** Unfortunately, these are the tasks requirements:
-   * If AI assistant page:
-   * simple tap - audio controls
-   * long tap - upload file
-   * If NOT AI assistant page:
-   * simple tap - upload file
-   * long tap - audio controls
-   */
-
   const bindPage = useLongPress(runRecording, {
     onCancel: uploadFile,
-    threshold: 500,
-    captureEvent: true,
-    cancelOnMovement: true
-  });
-
-  const bindAssistantPage = useLongPress(uploadFile, {
-    onCancel: runRecording,
     threshold: 500,
     captureEvent: true,
     cancelOnMovement: true
@@ -81,17 +59,12 @@ export default function MainButton() {
       )}>
       {isRecording || isSpeaking ? (
         <button className={styles['main-button']} onClick={stopAllAudioActions}>
-          {isRecording && <RecordButtonEqualizer />}
+          {/* {isRecording && <RecordButtonEqualizer />} */}
           <StopRecordIcon />
         </button>
       ) : isResponseGenerating ? (
         <button className={styles['main-button']}>
           <LoadingSvg />
-        </button>
-      ) : isAssistantPage ? (
-        <button className={styles['main-button']} {...bindAssistantPage()}>
-          <UploadAction ref={uploadRef} />
-          <div className={styles.overlay}></div>
         </button>
       ) : (
         <button className={styles['main-button']} {...bindPage()}>
